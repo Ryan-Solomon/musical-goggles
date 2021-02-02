283b09a0a8778eada89a31688b1f5613a240e5ba
Ryan-Solomon <ryanjsolomon24@gmail.com>
2021-02-02 17:23:52 -0600

    add homePage

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,16 @@ import './App.css';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import { TodoPage } from './pages/TodoPage';
 import { Navigation } from './components/Nav';
+import { HomePage } from './pages/HomePage';
 
 export const App = () => {
   return (
     <Router>
       <Navigation />
       <Switch>
+        <Route exact path='/'>
+          <HomePage />
+        </Route>
         <Route exact path='/todos'>
           <TodoPage />
         </Route>

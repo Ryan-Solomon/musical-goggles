7eca8b9bf3bd907b5a1a3a76409be4176bb81165
Ryan-Solomon <ryanjsolomon24@gmail.com>
2021-02-02 16:35:38 -0600

    add todopage

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,16 @@
 import React from 'react';
 import './App.css';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { TodoPage } from './pages/TodoPage';
 
 export const App = () => {
-  return <h1>App</h1>;
+  return (
+    <Router>
+      <Switch>
+        <Route exact path='/todos'>
+          <TodoPage />
+        </Route>
+      </Switch>
+    </Router>
+  );
 };
--- src/pages/TodoPage.tsx
+++ src/pages/TodoPage.tsx
@@ -1,5 +1,10 @@
 import React from 'react';
+import { Todo } from '../components/Todo';
 
 export const TodoPage = () => {
-  return <></>;
+  return (
+    <>
+      <Todo />
+    </>
+  );
 };

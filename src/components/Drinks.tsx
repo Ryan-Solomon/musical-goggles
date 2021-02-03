6822d7eee36b0fd609b919a7e2ebc9e06682f9a6
Ryan-Solomon <ryanjsolomon24@gmail.com>
2021-02-03 17:43:44 -0600

    fetch data from cocktails db using swr

--- src/components/Drinks.tsx
+++ src/components/Drinks.tsx
@@ -1,6 +1,16 @@
 import React from 'react';
+import useSWR from 'swr';
+
+const fetcher = (url: string) => fetch(url).then((res) => res.json());
 
 export const Drinks = () => {
+  const { data, error } = useSWR(
+    'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita',
+    fetcher
+  );
+  if (error) return <div>failed to load</div>;
+  if (!data) return <div>loading...</div>;
+  console.log(data);
   return (
     <>
       <h1>Drinks</h1>

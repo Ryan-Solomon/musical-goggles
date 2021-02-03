250057a0e0ce7e5931f34623c73908820770c177
Ryan-Solomon <ryanjsolomon24@gmail.com>
2021-02-03 16:32:38 -0600

    style todos

--- src/components/Todos.tsx
+++ src/components/Todos.tsx
@@ -74,41 +74,76 @@ export const Todos = () => {
 
   return (
     <Container>
-      <form onSubmit={handleSubmit}>
-        <Label>Add Todo</Label>
-        <Input
-          value={todoTextInput}
-          onChange={(e) => setTodoTextInput(e.target.value)}
-          type='text'
-        />
-        <Button type='submit'>Add</Button>
-      </form>
-      {state.todos.length === 0 ? (
-        <h1>Yo, add some todos</h1>
-      ) : (
-        state.todos.map((todo) => {
-          return (
-            <div key={todo.id}>
-              <Todo todo={todo} />
-              <Button onClick={() => removeTodo(todo.id)}>X</Button>
-            </div>
-          );
-        })
-      )}
-      {state.todos.length > 0 && <Button onClick={clearTodos}>Clear</Button>}
+      <Content>
+        <Form onSubmit={handleSubmit}>
+          <Label>Add Todo</Label>
+          <FormInputAndButton>
+            <Input
+              placeholder='Something to do..'
+              value={todoTextInput}
+              onChange={(e) => setTodoTextInput(e.target.value)}
+              type='text'
+            />
+            <Button type='submit'>Add</Button>
+          </FormInputAndButton>
+        </Form>
+        {state.todos.length === 0 ? (
+          <h1>Yo, add some todos</h1>
+        ) : (
+          state.todos.map((todo) => {
+            return (
+              <div key={todo.id}>
+                <Todo todo={todo} />
+                <Button onClick={() => removeTodo(todo.id)}>X</Button>
+              </div>
+            );
+          })
+        )}
+        {state.todos.length > 0 && <Button onClick={clearTodos}>Clear</Button>}
+      </Content>
     </Container>
   );
 };
 
-const Container = styled.div``;
+const Container = styled.div`
+  display: grid;
+  place-items: center;
+  height: 80vh;
+`;
+
+const Content = styled.div``;
+
+const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+`;
+
+const FormInputAndButton = styled.div`
+  margin-bottom: 2rem;
+`;
 
 const Label = styled.label`
-  font-size: 2rem;
+  text-align: center;
+  font-size: 2.5rem;
+  color: white;
+  margin-bottom: 2rem;
 `;
 const Input = styled.input`
-  padding: 0.5rem;
+  padding: 0.7rem;
+  font-size: 2rem;
 `;
 
 const Button = styled.button`
-  padding: 0.5rem;
+  padding: 0.7rem;
+  font-size: 2rem;
+  background: none;
+  border: 1px solid white;
+  color: white;
+  margin-left: 2rem;
+  transition: all 0.2s ease;
+  &:hover {
+    transform: scale(1.05);
+    cursor: pointer;
+    border: 1px solid green;
+  }
 `;

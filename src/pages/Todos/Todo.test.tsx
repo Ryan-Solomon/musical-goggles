a0ed9628c4706f9f2073c9b2ecd1bb8dc1cdb592
Ryan-Solomon <ryanjsolomon24@gmail.com>
2021-02-03 17:20:20 -0600

    test add and remove todo

--- src/pages/Todos/Todo.test.tsx
+++ src/pages/Todos/Todo.test.tsx
@@ -1,10 +1,21 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import { TodoPage } from './TodoPage';
+import userEvent from '@testing-library/user-event';
 
 describe('Todo page', () => {
   test('renders todo page', () => {
     render(<TodoPage />);
     expect(screen.getByText(/add some todos/i)).toBeInTheDocument();
+  });
+  test('Add todo', () => {
+    render(<TodoPage />);
+    userEvent.type(screen.getByRole('textbox'), 'todotests');
+    userEvent.click(screen.getByRole('button'));
+    expect(screen.getByText('todotests')).toBeInTheDocument();
+    userEvent.click(screen.getAllByRole('button')[1]);
+    expect(screen.getByText(/add some todos/i)).toBeInTheDocument();
+
+    screen.debug();
   });
 });

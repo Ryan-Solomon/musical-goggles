d945565bbb6ad67207422da91b77a3aebb67c1b1
Ryan-Solomon <ryanjsolomon24@gmail.com>
2021-02-04 14:00:39 -0600

    test on click is called once when clicking on instructions

--- src/pages/Drinks/Drinks.test.tsx
+++ src/pages/Drinks/Drinks.test.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { queryByText, render, screen, waitFor } from '@testing-library/react';
 import { DrinksPage } from './DrinksPage';
 import userEvent from '@testing-library/user-event';
+import { MemoryRouter } from 'react-router-dom';
 
 describe('Drinks page', () => {
   test('renders Drinks page', () => {
@@ -16,5 +17,13 @@ describe('Drinks page', () => {
     });
 
     expect(screen.getAllByText(/margarita/i)[0]).toBeInTheDocument();
+  });
+  test('load instructions', () => {
+    render(<DrinksPage />, { wrapper: MemoryRouter });
+    const onClickFn = jest.fn();
+    const instructions = screen.getAllByText('Show Instructions')[0];
+    instructions.onclick = onClickFn;
+    userEvent.click(instructions);
+    expect(onClickFn).toHaveBeenCalledTimes(1);
   });
 });

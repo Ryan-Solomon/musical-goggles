6becaca95ba63e48f69defa3f12b6f667aa21cce
Ryan-Solomon <ryanjsolomon24@gmail.com>
2021-02-03 18:02:24 -0600

    style drink component

--- src/components/Drink.tsx
+++ src/components/Drink.tsx
@@ -1,4 +1,5 @@
 import React, { FC } from 'react';
+import styled from 'styled-components';
 import { TDrink } from './Drinks';
 
 type TProps = {
@@ -8,8 +9,30 @@ type TProps = {
 export const Drink: FC<TProps> = ({ drink }) => {
   const { strDrink, strDrinkThumb } = drink;
   return (
-    <>
-      <h1>{strDrink}</h1>
-    </>
+    <DrinkContainer>
+      <DrinkTitle>{strDrink}</DrinkTitle>
+      <DrinkImage src={strDrinkThumb} alt='drink' />
+    </DrinkContainer>
   );
 };
+
+const DrinkContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  box-shadow: 2px 2px 10px #8f8f8f;
+  height: 300px;
+  overflow: hidden;
+`;
+
+const DrinkTitle = styled.h2`
+  text-align: center;
+  color: white;
+  font-size: 2rem;
+  padding: 1.5rem;
+  letter-spacing: 0.2rem;
+`;
+
+const DrinkImage = styled.img`
+  width: 100%;
+  object-fit: cover;
+`;

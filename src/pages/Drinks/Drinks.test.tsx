0b71385cc3348c125e878302d3a868f519ff425a
Ryan-Solomon <ryanjsolomon24@gmail.com>
2021-02-04 13:45:56 -0600

    test loading of drinks

--- src/pages/Drinks/Drinks.test.tsx
+++ src/pages/Drinks/Drinks.test.tsx
@@ -1,12 +1,20 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { queryByText, render, screen, waitFor } from '@testing-library/react';
 import { DrinksPage } from './DrinksPage';
 import userEvent from '@testing-library/user-event';
 
 describe('Drinks page', () => {
   test('renders Drinks page', () => {
     render(<DrinksPage />);
-    expect(screen.getByText(/loading/)).toBeInTheDocument();
-    screen.debug();
+    expect(screen.getByText(/loading/i)).toBeInTheDocument();
+  });
+  test('loads drinks', async () => {
+    render(<DrinksPage />);
+    expect(screen.getByText(/loading/i)).toBeInTheDocument();
+    await waitFor(() => {
+      expect(screen.queryByText(/loading/i)).not.toBeInTheDocument();
+    });
+
+    expect(screen.getByText(/drinks/i)).toBeInTheDocument();
   });
 });

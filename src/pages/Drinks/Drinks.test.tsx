75aba313b78409434e7a843fd5104fa081ad6f47
Ryan-Solomon <ryanjsolomon24@gmail.com>
2021-02-03 19:54:22 -0600

    setup tests for drinks

---
+++ src/pages/Drinks/Drinks.test.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import { DrinksPage } from './DrinksPage';
+import userEvent from '@testing-library/user-event';
+
+describe('Drinks page', () => {
+  test('renders Drinks page', () => {
+    render(<DrinksPage />);
+  });
+});

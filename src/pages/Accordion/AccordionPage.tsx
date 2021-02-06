a78a9f88443dfb7fe734545e783afe2fc3cc3855
Ryan-Solomon <ryanjsolomon24@gmail.com>
2021-02-05 19:48:27 -0600

    import acc to accPage

--- src/pages/Accordion/AccordionPage.tsx
+++ src/pages/Accordion/AccordionPage.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
+import { Accordion } from '../../components/Accordion';
 
 export const AccordionPage = () => {
-  return <>Accordion Page</>;
+  return <Accordion />;
 };

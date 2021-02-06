3cdf32bfdf16182ba37634fac2de6897b3e8004e
Ryan-Solomon <ryanjsolomon24@gmail.com>
2021-02-05 19:50:32 -0600

    setup base styles for acc

--- src/components/Accordion.tsx
+++ src/components/Accordion.tsx
@@ -1,5 +1,14 @@
 import React from 'react';
+import styled from 'styled-components';
 
 export const Accordion = () => {
   return <h1>Accordion</h1>;
 };
+
+const Container = styled.div``;
+
+const Content = styled.div``;
+
+const Title = styled.h1``;
+
+const ItemContainer = styled.div``;

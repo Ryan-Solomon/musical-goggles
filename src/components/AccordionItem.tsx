c37950667b396f9cbabf35fc6fc8be2575c82e21
Ryan-Solomon <ryanjsolomon24@gmail.com>
2021-02-05 19:55:12 -0600

    setup accItem

---
+++ src/components/AccordionItem.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styled from 'styled-components';
+
+export const AccordionItem = () => {
+  return <h1>ACCITEM</h1>;
+};
+
+export const Container = styled.div``;
+
+export const Text = styled.h2``;
+
+export const Icon = styled.h3``;
